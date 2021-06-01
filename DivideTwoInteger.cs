311944c3f8d819b7dae55c5e7229c9a3f4ba2dcf
Erfan Rafezi <53901928+ErfanRFZ@users.noreply.github.com>
2021-06-01 22:09:56 +0300

    Add files via upload

---
+++ DivideTwoInteger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class DivideTwoInteger
+{
+    public class Solution
+    {
+
+        public int Divide(int dividend, int divisor)
+        {
+
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            long result = (long)dividend / (long)divisor;
+
+            if (result < int.MinValue || result > int.MaxValue)
+            {
+                return (int)Math.Pow(2, 31) - 1;
+            }
+
+            return (int)result;
+        }
+    }
+}
